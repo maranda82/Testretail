ffb52d3fa9873e2c4c6d1806f39f4ea2bd32de66
maranda82 <beto_aranda@hotmail.com>
2021-07-01 20:46:01 -0500

    Create TestRetail.java
    Test alghorithm

---
+++ com.testretail/TestRetail.java
@@ -0,0 +1,8 @@
+package com.testretail;
+
+public class TestRetail{
+    public static void main(String[] args) {
+        Integer integer = new TestRetail()
+        System.out.println("Hello, World !!!");
+    }
+}
